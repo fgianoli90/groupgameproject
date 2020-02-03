280190e32d37d0f5bba3f5cc046d1186ab79c055
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2020-02-03 12:07:21 -0500

    adding and cleaning up code

--- first_game/canvas.js
+++ first_game/canvas.js
@@ -1,9 +1,41 @@
+// document.querySelector('#start-button').onclick = function(){
+//     this.remove(); //removes start button
+//     startGame(); //calls startGame  
+//   };
 var canvas = document.querySelector('canvas');  // associates canvas tag with canvas new var
 
 canvas.width = window.innerWidth; // Sets our canvas to browsers current dimensions
 canvas.height = window.innerHeight;
 
 var ctx = canvas.getContext('2d');
+var img = new Image(); //load an image element
+
+function startGame(){
+    console.log("START");
+    img.onload = function() {  //Load the car for the first time 
+       ctx.drawImage(img, person.x, person.y, person.width, person.height);
+    };
+    img.src = "./images/ship.gif";
+  
+    window.requestAnimationFrame(animate) //Starts the animation infinite loop
+  }
+
+
+
+let person = {  
+    x:canvas.width / 2,
+    y:canvas.height * 6/7,
+    width: 50,
+    height: 80
+  }
+  
+  
+//   function drawPerson() {
+//     ctx.drawImage(img, person.x, person.y, person.width, person.height); //draws the player depending on the coords in the obj above 
+//   }
+
+
+
 
 //Rectangles
 // ctx.fillStyle= 'rgba(255,0,0,0.5)'
@@ -34,22 +66,39 @@ var ctx = canvas.getContext('2d');
 //     ctx.stroke();
 // }
 
+
+
 var mouse = {
     x: undefined,
     y: undefined
 }
 
-var maxRadius = 40;
-var minRadius = 2;
+// var maxRadius = 40; relevant to shrinking and growing affect
+// var minRadius = 5;
 
-var colorArray = [
-    '#0A1747',
-    '#0029FA',
-    '#8D07F6',
-    '#FFFF05',
-    '#D4DBF5'
-];
+var colorArray = [];
 
+// colorArray[0] = new Image();
+// colorArray[0].src = './images/smiley.png';
+
+let smiley = new Image();
+smiley.src = './images/smiley.png';
+
+
+// colorArray[1] = new Image();
+// colorArray[1].src = './images/smiley.png';
+
+// colorArray[2] = new Image();
+// colorArray[2].src = './images/smiley.png';
+
+// colorArray[3] = new Image();
+// colorArray[3].src = './images/smiley.png';
+
+// colorArray[4] = new Image();
+// colorArray[4].src = './images/smiley.png';
+
+// colorArray[5] = new Image();
+// colorArray[5].src = './images/smiley.png';
 
 
 
@@ -68,6 +117,13 @@ window.addEventListener('resize', function(){
     init();
     })
 
+let gameWidth = canvas.width
+let gameHeight = canvas.height
+
+
+
+
+
 
 function Circle(x, y, dx, dy, radius){
         this.x = x;
@@ -78,14 +134,16 @@ function Circle(x, y, dx, dy, radius){
         this.minRadius = radius;
         this.stroke = colorArray[Math.floor(Math.random() * colorArray.length)];
         this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+        this.image = smiley 
         this.draw = function () {
             ctx.beginPath();
             ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
             // ctx.strokeStyle = 'blue';
             ctx.stroke();
             ctx.strokeStyle = this.stroke;
-            ctx.fillStyle = this.color;
-            ctx.fill();
+            // ctx.fillStyle = this.color;
+            // ctx.fill();
+            ctx.drawImage(this.image, this.x - 24, this.y - 24, 50, 50)
         };
         this.update = function(){
             if (this.x + this.radius > innerWidth || this.x - this.radius < 0){   
@@ -97,14 +155,16 @@ function Circle(x, y, dx, dy, radius){
             this.x += this.dx;
             this.y += this.dy;
 
-            if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y -this.y > -50){
-                if(this.radius < maxRadius){
-                    this.radius += 1;
-                }
+
+            // this part of our code has the zoom-in and zoom out affect
+            // if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y -this.y > -50){  
+            //     if(this.radius < maxRadius){
+            //         this.radius += 1;
+            //     }
                     
-            } else if (this.radius > minRadius){
-                this.radius -= 1;
-            }
+            // } else if (this.radius > minRadius){
+            //     this.radius -= 1;
+            // }
             this.draw();
         }
     }
@@ -116,18 +176,18 @@ console.log(circleArray)
 
 //declare animate function and request itself to create the loop
 var circleArray = [];
-function init(){
-   
 
-for (var i = 0; i < 800; i++){
-    var radius = Math.random() * 9 + 1;
-    var x = Math.random() * (innerWidth - radius * 2) + radius;   //random location for each spawn
-    var y = Math.random() * (innerHeight - radius * 2) + radius;
-    var dx = Math.random() - 0.5 * 2;   //create velocity variable
-    var dy = Math.random() - 0.5 * 2;  //randomize initial direction
-     // create a radius var so the bouce off the wall is cleaner
-    circleArray.push(new Circle(x, y, dx, dy, radius));
-}
+function init(){
+   for (var i = 0; i < 100; i++){
+        var radius = Math.random() * 9 + 9;
+        var x = Math.random() * (innerWidth - radius * 2) + radius;   //random location for each spawn
+        var y = Math.random() * ((innerHeight- 300) - radius * 2) + radius;
+        var dx = Math.random() - 0.5 * 2;   //create velocity variable
+        var dy = Math.random() - 0.5 * 2;  //randomize initial direction
+        // create a radius var so the bouce off the wall is cleaner
+        circleArray.push(new Circle(x, y, dx, dy, radius));
+    }
+    console.log(circleArray)
 }
 
     function animate(){
@@ -139,3 +199,6 @@ for (var i = 0; i < 800; i++){
     }
 init();
 animate();
+
+
+
